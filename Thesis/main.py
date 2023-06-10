c3508378058eea72f53701fd0bcdc256113a24b9
HUQiancheng <qianchenghu.tongji@gmail.com>
2023-06-10 15:30:31 +0200

    levels

--- Thesis/main.py
+++ Thesis/main.py
@@ -4,52 +4,57 @@ import random
 import csv
 from PIL import Image, ImageTk
 
-def main():
-    # Get the file path of the current script and the root folder path
-    current_file_path = os.path.abspath(__file__)
-    root_folder_path = os.path.dirname(os.path.dirname(current_file_path))
+class DifficultySelection:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Select Difficulty")
+        self.master.geometry("300x150")
 
-    # Get the path to the Images folder and the CSV file
-    images_folder_path = os.path.join(root_folder_path, 'Thesis', 'Images')
-    csv_file_path = os.path.join(images_folder_path, 'file.csv')
+        self.difficulty = tk.StringVar()
+        self.difficulty.set("easy")
 
-    # Create the CSV file if it doesn't exist and save image information to it
-    if not os.path.exists(csv_file_path):
-        with open(csv_file_path, 'w') as f:
-            f.write('Header1,Header2,Header3\n')
-        save_image_info_to_csv(csv_file_path)
+        easy_button = tk.Radiobutton(self.master, text="Easy", variable=self.difficulty, value="easy")
+        easy_button.pack(pady=5)
 
-    # Create the GUI figure
-    root = tk.Tk()
-    app = GUIFigure(root, csv_file_path)
-    root.mainloop()
+        medium_button = tk.Radiobutton(self.master, text="Medium", variable=self.difficulty, value="medium")
+        medium_button.pack(pady=5)
 
-def save_image_info_to_csv(csv_file_path):
-    # Get the path to the Images folder
-    images_folder_path = os.path.join(os.path.dirname(csv_file_path), 'Animals')
+        hard_button = tk.Radiobutton(self.master, text="Hard", variable=self.difficulty, value="hard")
+        hard_button.pack(pady=5)
 
-    # Get a list of all image files in the Images folder
-    image_files = [os.path.join(images_folder_path, f) for f in os.listdir(images_folder_path) if f.endswith('.jpg')]
+        start_button = tk.Button(self.master, text="Start", command=self.start_quiz)
+        start_button.pack(pady=10)
 
-    # Shuffle the image files
-    random.shuffle(image_files)
-
-    # Save image information to CSV file
-    with open(csv_file_path, 'a', newline='') as f:
-        writer = csv.writer(f)
-        for image_file in image_files:
-            category = os.path.basename(os.path.dirname(image_file))
-            writer.writerow([image_file, category])
+    def start_quiz(self):
+        self.master.destroy()
+        root = tk.Tk()
+        app = GUIFigure(root, self.difficulty.get())
+        root.mainloop()
 
 class GUIFigure:
-    def __init__(self, master, csv_file):
+    def __init__(self, master, difficulty):
         self.master = master
         self.master.title("Image Quiz")
         self.master.geometry("500x500")
+        self.difficulty = difficulty
+
+        # Get the file path of the current script and the root folder path
+        current_file_path = os.path.abspath(__file__)
+        root_folder_path = os.path.dirname(os.path.dirname(current_file_path))
+
+        # Get the path to the Images folder and the CSV file
+        images_folder_path = os.path.join(root_folder_path, 'Thesis', 'Images')
+        csv_file_path = os.path.join(images_folder_path, 'file.csv')
+
+        # Create the CSV file if it doesn't exist and save image information to it
+        if not os.path.exists(csv_file_path):
+            with open(csv_file_path, 'w') as f:
+                f.write('Header1,Header2,Header3\n')
+            save_image_info_to_csv(csv_file_path)
 
         # Load image data from CSV file
         self.image_data = []
-        with open(csv_file, "r") as f:
+        with open(csv_file_path, "r") as f:
             reader = csv.reader(f)
             next(reader) # Skip header row
             for row in reader:
@@ -76,6 +81,7 @@ class GUIFigure:
         self.image_path = ""
         self.image_name = ""
         self.num_questions = 0
+        self.difficulty = difficulty
         self.next_question()
 
     def next_question(self):
@@ -97,11 +103,11 @@ class GUIFigure:
         if len(image_name_options) == 0:
             image_name_options = [os.path.basename(row[0]) for row in self.image_data if row[1] != category and os.path.basename(row[0]) != self.image_name]
         random.shuffle(image_name_options)
-
-        # One of them is from the same category as the image, the other is from a different category
+        # easy: three options are from the different categories
+        # medium: One of them is from the same category as the image, the other is from a different category
         incorrect_option_1 = image_name_options[0]
         incorrect_option_2 = os.path.basename(random.choice([row[0] for row in self.image_data if row[1] != category and os.path.basename(row[0]) != self.image_name]))
-
+        # hard: three options are from the same category
         # Display answer options on buttons
         options = [correct_option, incorrect_option_1, incorrect_option_2]
         random.shuffle(options)
@@ -128,5 +134,25 @@ class GUIFigure:
 
         # Move on to next question
         self.next_question()
+
+def save_image_info_to_csv(csv_file_path):
+    # Get the path to the Images folder
+    images_folder_path = os.path.join(os.path.dirname(csv_file_path), 'Animals')
+
+    # Get a list of all image files in the Images folder
+    image_files = [os.path.join(images_folder_path, f) for f in os.listdir(images_folder_path) if f.endswith('.jpg')]
+
+    # Shuffle the image files
+    random.shuffle(image_files)
+
+    # Save image information to CSV file
+    with open(csv_file_path, 'a', newline='') as f:
+        writer = csv.writer(f)
+        for image_file in image_files:
+            category = os.path.basename(os.path.dirname(image_file))
+            writer.writerow([image_file, category])
+
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    app = DifficultySelection(root)
+    root.mainloop()
